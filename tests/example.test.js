c93d18de7e4f585ae5d66a6b1cf1d1187fdc5d52
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-02-04 10:11:55 +0700

    Jest with puppeteer

---
+++ tests/example.test.js
@@ -0,0 +1,13 @@
+describe('Google homepage', () => {
+  beforeAll(async () => {
+    jest.setTimeout(15000)
+    await page.goto('https://google.com')
+  });
+
+  it('should load google homepage', async () => {
+    await page.waitForSelector('input[name="q"]')
+    await jestPuppeteer.debug()
+    let title = await page.title()
+    console.log("TITLE IS : " + title)
+  })
+})
